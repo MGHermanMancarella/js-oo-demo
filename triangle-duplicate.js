0030cfe5a241ce96a79e7a0b05e164f996bea4f0
Michael <mherman315@gmail.com>
2023-04-06 11:41:50 -0700

    initial

---
+++ triangle-duplicate.js
@@ -0,0 +1,19 @@
+class Triangle {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  getArea() {
+    return (this.a * this.b) / 2;
+  }
+
+  getHypotenuse() {
+    return Math.sqrt(
+        this.a ** 2 + this.b ** 2);
+  }
+
+  describe() {
+    return `Area is ${this.getArea()}.`;
+  }
+}
---
+++ triangle-oo.js
@@ -0,0 +1,11 @@
+class Triangle {
+
+  getArea() {
+    return (this.a * this.b) / 2;
+  }
+
+  getHypotenuse() {
+    return Math.sqrt(this.a ** 2 + this.b ** 2);
+  }
+}
+// end
